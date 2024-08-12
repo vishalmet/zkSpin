dd1acf04e56c7d3edc84dad39efa33949d728a7b
vishalmet <vishalmet003@gmail.com>
2024-08-12 19:39:10 +0530

    spin

--- src/components/SpinPlayground.jsx
+++ src/components/SpinPlayground.jsx
@@ -5,7 +5,7 @@ import Game4 from "../assets/game4.png"
 
 const SpinPlayground = () => {
   return (
-    <div className='pt-20 mx-20'>
+    <div className='pt-24 mx-20'>
         <p className=' text-7xl space-grotesk text-center font-medium bg-gradient-to-b from-customStart to-customEnd bg-clip-text text-transparent'>SPIN PLAYGROUND</p>
         <p className=' text-center'>Super Lightweight Retro Spin Games 🚀</p>
         <div className=" pt-10 grid grid-cols-3">
