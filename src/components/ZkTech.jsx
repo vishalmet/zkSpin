ead08a4794ccbf501e270df2c18d9c7a48ebe2b4
vishalmet <vishalmet003@gmail.com>
2024-08-13 11:43:36 +0530

    zktech res

--- src/components/ZkTech.jsx
+++ src/components/ZkTech.jsx
@@ -3,13 +3,13 @@ import Flow from "../assets/flow.png"
 
 const ZkTech = () => {
   return (
-    <div className=" bg-gradient-to-br from-customStart to-customEnd mx-20 mt-20 h-full p-16 text-white">
-      <div className=" flex space-x-32 pt-10">
-        <p className=" uppercase text-7xl font-medium space-grotesk">
+    <div className=" bg-gradient-to-br from-customStart to-customEnd mx-4 md:mx-20 mt-10 md:mt-20 h-full p-4 md:p-16 text-white">
+      <div className=" md:flex md:space-x-32 pt-4 md:pt-10">
+        <p className=" uppercase text-4xl md:text-5xl lg:text-7xl font-medium space-grotesk">
           the zkspin <br /> technology
         </p>
-        <div className=" px-10">
-          <p className=" text-2xl">
+        <div className=" px-1 pt-2 md:pt-0 md:px-10">
+          <p className=" text-xs md:text-2xl">
             As the first project to proposed verifiable gaming in August, 2023,
             zkSpin leverages computing technologies developed at MIT and
             Berkeley to deliver the ultimate immersive on-chain gaming
@@ -18,13 +18,13 @@ const ZkTech = () => {
             developer community to try out this new technology, we make
             developer experience the top priority in our long term roadmap.
           </p>
-          <div className=" pt-6 space-x-6">
-            <button className=" p-4 bg-black hover:bg-gray-700 font-semibold"> LIVE PAPER (COMING SOON)</button>
-            <button className=" p-4 bg-black hover:bg-gray-700 font-semibold"> ZKP PRIMER</button>
+          <div className=" pt-6 md:space-x-6">
+            <button className=" p-2 md:p-4 bg-black text-base hover:bg-gray-700 hover:scale-95 font-semibold"> LIVE PAPER (COMING SOON)</button>
+            <button className=" mt-2 md:mt-0 p-2 md:p-4 bg-black hover:bg-gray-700 hover:scale-95 font-semibold"> ZKP PRIMER</button>
           </div>
         </div>
       </div>
-      <div className=" pt-16">
+      <div className=" pt-4 md:pt-16">
         <img src={Flow} alt="" />
       </div>
     </div>
