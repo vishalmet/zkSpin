5c53c9b7960dc85870b5a77fd998b3b1e65cf4cd
vishalmet <vishalmet003@gmail.com>
2024-08-16 15:00:34 +0530

    nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,9 +20,9 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center">
-            <div className="text-lg font-bold">
+            <Link to="/" className="text-lg font-bold">
               <img className="h-8" src={Logo} alt="logo" />
-            </div>
+            </Link>
           </div>
           <div className="hidden md:block">
             <div className="flex text-[18px] items-baseline space-x-10">
