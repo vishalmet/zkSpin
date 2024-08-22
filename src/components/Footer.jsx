5e12baee498d6439b2cec6bbbd87cfb72dee9481
vishalmet <vishalmet003@gmail.com>
2024-08-22 22:46:11 +0530

    onclick, scroll up

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,26 +3,33 @@ import Logo2 from "../assets/logowhite.png";
 import Discord from "../assets/icons/Discord.png";
 import Telegram from "../assets/icons/Telegram.png";
 import Twitter from "../assets/icons/Twitter.png";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Footer = () => {
+  const navigate = useNavigate();
+
+  const handleNavigation = (path) => {
+    navigate(path);
+    window.scrollTo(0, 0); // Scroll to the top after navigation
+  };
+
   return (
-    <div className=" bg-gradient-to-br from-customStart to-customEnd h-full mt-24">
+    <div className="bg-gradient-to-br from-customStart to-customEnd h-full mt-24">
       <div className="mx-4 md:mx-20 py-10 md:py-20 md:flex">
-        <div className=" flex-1 pb-10 md:pb-0">
-          <img className=" w-[155px] h-[40px]" src={Logo2} alt="" />
+        <div className="flex-1 pb-10 md:pb-0">
+          <img className="w-[155px] h-[40px]" src={Logo2} alt="logo" />
         </div>
         <div className="pt-2 space-y-4 md:space-y-0 md:flex md:space-x-32">
-          <div className=" flex justify-center space-x-16 md:space-x-32">
-            <div className="">
+          <div className="flex justify-center space-x-16 md:space-x-32">
+            <div>
               <p className="font-semibold text-xl">ZKSPIN</p>
               <ol>
-                <Link to="/">Tech</Link> <br />
-                <Link to="/games">Games</Link> <br />
-                <Link to="/playground">Playground</Link>
+                <button onClick={() => handleNavigation("/")}>Tech</button> <br />
+                <button onClick={() => handleNavigation("/games")}>Games</button> <br />
+                <button onClick={() => handleNavigation("/playground")}>Playground</button>
               </ol>
             </div>
-            <div className="">
+            <div>
               <p className="font-semibold text-xl">RESOURCES</p>
               <ol>
                 <a
@@ -38,34 +45,22 @@ const Footer = () => {
               </ol>
             </div>
           </div>
-          <div className=" flex justify-center">
-            <div className="">
+          <div className="flex justify-center">
+            <div>
               <p className="font-semibold text-xl">ABOUT</p>
-              <div className=" flex gap-3">
+              <div className="flex gap-3">
                 <a href="" target="_blank" rel="noopener noreferrer">
-                  <img
-                    className=" h-[24px] w-[24px]"
-                    src={Discord}
-                    alt="discord"
-                  />
+                  <img className="h-[24px] w-[24px]" src={Discord} alt="discord" />
                 </a>
                 <a href="" target="_blank" rel="noopener noreferrer">
-                  <img
-                    className=" h-[24px] w-[24px] "
-                    src={Telegram}
-                    alt="telegram"
-                  />
+                  <img className="h-[24px] w-[24px]" src={Telegram} alt="telegram" />
                 </a>
                 <a
                   href="https://x.com/zkspin"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
-                  <img
-                    className=" h-[24px] w-[24px]"
-                    src={Twitter}
-                    alt="twitter"
-                  />
+                  <img className="h-[24px] w-[24px]" src={Twitter} alt="twitter" />
                 </a>
               </div>
             </div>
