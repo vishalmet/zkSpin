318a4421ef68fb11a887034d49bd6373e3798f1b
vishalmet <vishalmet003@gmail.com>
2024-08-22 22:13:11 +0530

    mobile header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
   return (
     <nav className="gradient-container overflow-x-hidden epilogue text-white">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex items-center justify-between h-16">
+        <div className="flex items-center justify-between h-20">
           <div className="flex items-center">
             <Link to="/" className="text-lg font-bold">
               <img className="h-8" src={Logo} alt="logo" />
@@ -74,7 +74,12 @@ const Navbar = () => {
               </a>
               <div className="hidden md:block">
                 <div className=" flex gap-6">
-                  <a  href="https://github.com/m4-team/spin-sdk/blob/main/guide/2_INTRODUCTION.md" target="_blank" rel="noopener noreferrer" class="Btn-Container">
+                  <a
+                    href="https://github.com/m4-team/spin-sdk/blob/main/guide/2_INTRODUCTION.md"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="Btn-Container"
+                  >
                     <span class="text">Github</span>
                     <span class="icon-Container">
                       <svg
@@ -147,7 +152,12 @@ const Navbar = () => {
                       </svg>
                     </span>
                   </a>
-                  <a href="https://thirumurugan7.hashnode.dev/spin-up-an-onchain-game-with-zk-spin?ref=twitter-share" target="_blank" rel="noopener noreferrer" class="Btn-Container">
+                  <a
+                    href="https://thirumurugan7.hashnode.dev/spin-up-an-onchain-game-with-zk-spin?ref=twitter-share"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    class="Btn-Container"
+                  >
                     <span class="text">Docs</span>
                     <span class="icon-Container">
                       <svg
@@ -315,134 +325,20 @@ const Navbar = () => {
           >
             Contact Us
           </a>
-          <div className=" px-2">
-            <div className=" space-y-4">
-              <a href="https://github.com/m4-team/spin-sdk/blob/main/guide/2_INTRODUCTION.md" target="_blank" rel="noopener noreferrer" class="Btn-Container">
-                <span class="text">Github</span>
-                <span class="icon-Container">
-                  <svg
-                    width="16"
-                    height="19"
-                    viewBox="0 0 16 19"
-                    fill="nones"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <circle
-                      cx="1.61321"
-                      cy="1.61321"
-                      r="1.5"
-                      fill="black"
-                    ></circle>
-                    <circle
-                      cx="5.73583"
-                      cy="1.61321"
-                      r="1.5"
-                      fill="black"
-                    ></circle>
-                    <circle
-                      cx="5.73583"
-                      cy="5.5566"
-                      r="1.5"
-                      fill="black"
-                    ></circle>
-                    <circle
-                      cx="9.85851"
-                      cy="5.5566"
-                      r="1.5"
-                      fill="black"
-                    ></circle>
-                    <circle cx="9.85851" cy="9.5" r="1.5" fill="black"></circle>
-                    <circle cx="13.9811" cy="9.5" r="1.5" fill="black"></circle>
-                    <circle
-                      cx="5.73583"
-                      cy="13.4434"
-                      r="1.5"
-                      fill="black"
-                    ></circle>
-                    <circle
-                      cx="9.85851"
-                      cy="13.4434"
-                      r="1.5"
-                      fill="black"
-                    ></circle>
-                    <circle
-                      cx="1.61321"
-                      cy="17.3868"
-                      r="1.5"
-                      fill="black"
-                    ></circle>
-                    <circle
-                      cx="5.73583"
-                      cy="17.3868"
-                      r="1.5"
-                      fill="black"
-                    ></circle>
-                  </svg>
-                </span>
-              </a>
-              <a href="https://thirumurugan7.hashnode.dev/spin-up-an-onchain-game-with-zk-spin?ref=twitter-share" target="_blank" rel="noopener noreferrer" class="Btn-Container">
-                <span class="text">Docs</span>
-                <span class="icon-Container">
-                  <svg
-                    width="16"
-                    height="19"
-                    viewBox="0 0 16 19"
-                    fill="nones"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <circle
-                      cx="1.61321"
-                      cy="1.61321"
-                      r="1.5"
-                      fill="black"
-                    ></circle>
-                    <circle
-                      cx="5.73583"
-                      cy="1.61321"
-                      r="1.5"
-                      fill="black"
-                    ></circle>
-                    <circle
-                      cx="5.73583"
-                      cy="5.5566"
-                      r="1.5"
-                      fill="black"
-                    ></circle>
-                    <circle
-                      cx="9.85851"
-                      cy="5.5566"
-                      r="1.5"
-                      fill="black"
-                    ></circle>
-                    <circle cx="9.85851" cy="9.5" r="1.5" fill="black"></circle>
-                    <circle cx="13.9811" cy="9.5" r="1.5" fill="black"></circle>
-                    <circle
-                      cx="5.73583"
-                      cy="13.4434"
-                      r="1.5"
-                      fill="black"
-                    ></circle>
-                    <circle
-                      cx="9.85851"
-                      cy="13.4434"
-                      r="1.5"
-                      fill="black"
-                    ></circle>
-                    <circle
-                      cx="1.61321"
-                      cy="17.3868"
-                      r="1.5"
-                      fill="black"
-                    ></circle>
-                    <circle
-                      cx="5.73583"
-                      cy="17.3868"
-                      r="1.5"
-                      fill="black"
-                    ></circle>
-                  </svg>
-                </span>
-              </a>
+          <div className=" px-3 pb-4">
+            <div className="space-x-4 ">
+              <a
+                href="https://github.com/m4-team/spin-sdk/blob/main/guide/2_INTRODUCTION.md"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="customButton"
+              >Github</a>
+              <a
+                href="https://thirumurugan7.hashnode.dev/spin-up-an-onchain-game-with-zk-spin?ref=twitter-share"
+                target="_blank"
+                rel="noopener noreferrer"
+                class="customButton"
+              >Docs</a>
             </div>
           </div>
         </div>
