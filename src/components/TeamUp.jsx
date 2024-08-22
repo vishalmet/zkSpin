9113498c34ce044b1bc8d824b976f40dfd8fdf3a
vishalmet <vishalmet003@gmail.com>
2024-08-22 22:46:40 +0530

    changed the partners logos

--- src/components/TeamUp.jsx
+++ src/components/TeamUp.jsx
@@ -1,29 +1,38 @@
-import React from 'react'
-import Near from "../assets/brands/near.png"
-import DAOMaker from "../assets/brands/daomaker.png"
-import LDcapital from "../assets/brands/ldcapital.png"
-import Binatar from "../assets/brands/binatir.png"
-import CoinUnited from "../assets/brands/coinunited.png"
-import SnapFingers from "../assets/brands/snap.png"
-import Commas from "../assets/brands/3commas.png"
-import Mexc from "../assets/brands/mexc.png"
+import React from "react";
+
+import CSX from "../assets/partners/csx.png";
+import Rings from "../assets/partners/rings.png";
+import Doc from "../assets/partners/doc.png";
+import BGA from "../assets/partners/bga.png";
+import ABGA from "../assets/partners/abga.png";
+import Certik from "../assets/partners/certik.png";
+import Delphinus from "../assets/partners/delphinuslabs.png";
+import CircleTri from "../assets/partners/circletri.png";
+import Neural from "../assets/partners/neural.png";
+import Artela from "../assets/partners/artela.png";
 
 const TeamUp = () => {
   return (
-    <div className=' mx-4 md:mx-20 pt-10 md:pt-24'>
-        <p className='text-4xl md:text-5xl lg:text-7xl uppercase space-grotesk text-center font-medium bg-gradient-to-b from-customStart to-customEnd bg-clip-text text-transparent'>TEAMED UP WITH <br /> THE BEST</p>
-        <div className=" grid grid-cols-2 md:grid-cols-4 gap-6 items-center pt-10" >
-            <img className=' w-[357px] hover:scale-105' src={Near} alt="near foundations" />
-            <img className=' w-[330px] hover:scale-105' src={DAOMaker} alt="daomaker" />
-            <img className=' w-[410px] hover:scale-105' src={LDcapital} alt="ldcapital" />
-            <img className=' w-[272px] hover:scale-105' src={Binatar} alt="binatar" />
-            <img className=' w-[372px] hover:scale-105' src={CoinUnited} alt="coinunited" />
-            <img className=' w-[357px] hover:scale-105' src={SnapFingers} alt="snapfingers" />
-            <img className=' w-[235px] hover:scale-105' src={Commas} alt="3commas" />
-            <img className=' w-[235px] hover:scale-105' src={Mexc} alt="mexc" />
+    <div className="mx-4 md:mx-20 pt-10 md:pt-24">
+      <p className="text-4xl md:text-5xl lg:text-7xl uppercase space-grotesk text-center font-medium bg-gradient-to-b from-customStart to-customEnd bg-clip-text text-transparent">
+        TEAMED UP WITH <br /> THE BEST
+      </p>
+      <div className=" bg-gradient-to-tl from-customStart to-customEnd mt-10 md:mt-20 h-full p-4 md:p-16 items-center">
+        <div className=" grid grid-cols-2 md:grid-cols-5 gap-6 items-center">
+          <img className=" h-8 md:h-16 flex justify-center mx-auto hover:scale-105" src={CSX} alt="near foundations" />
+          <img className=" h-8 md:h-16 flex justify-center mx-auto hover:scale-105" src={Rings} alt="daomaker" />
+          <img className=" h-8 md:h-16 flex justify-center mx-auto hover:scale-105" src={Doc} alt="ldcapital" />
+          <img className=" h-8 md:h-16 flex justify-center mx-auto hover:scale-105" src={BGA} alt="binatar" />
+          <img className=" h-8 md:h-16 flex justify-center mx-auto hover:scale-105" src={ABGA} alt="coinunited" />
+          <img className=" h-8 md:h-16 flex justify-center mx-auto hover:scale-105" src={Certik} alt="snapfingers" />
+          <img className=" h-8 md:h-16 flex justify-center mx-auto hover:scale-105" src={Delphinus} alt="3commas" />
+          <img className=" h-8 md:h-16 flex justify-center mx-auto hover:scale-105" src={CircleTri} alt="mexc" />
+          <img className=" h-8 md:h-16 flex justify-center mx-auto hover:scale-105" src={Neural} alt="mexc" />
+          <img className=" h-8 md:h-16 flex justify-center mx-auto hover:scale-105" src={Artela} alt="mexc" />
         </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TeamUp
+export default TeamUp;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -63,7 +63,7 @@ const Home = () => {
           </div>
         </div>
       </div>
-
+      
       <ZkTech />
       <SpinIntelligence />
       <RoadMap />
