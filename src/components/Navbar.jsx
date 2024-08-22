f2ce63eb48b5f9f457d77a93c20bd01e58a208a7
vishalmet <vishalmet003@gmail.com>
2024-08-22 20:58:09 +0530

    added github and docs in the header

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
   const handleMenuClick = (menu) => {
     setActiveMenu(menu);
-    setIsOpen(false); 
+    setIsOpen(false);
   };
 
   const toggleMenu = () => {
@@ -72,6 +72,156 @@ const Navbar = () => {
               >
                 Contact Us
               </a>
+              <div className="hidden md:block">
+                <div className=" flex gap-6">
+                  <a  href="https://github.com/m4-team/spin-sdk/blob/main/guide/2_INTRODUCTION.md" target="_blank" rel="noopener noreferrer" class="Btn-Container">
+                    <span class="text">Github</span>
+                    <span class="icon-Container">
+                      <svg
+                        width="16"
+                        height="19"
+                        viewBox="0 0 16 19"
+                        fill="nones"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <circle
+                          cx="1.61321"
+                          cy="1.61321"
+                          r="1.5"
+                          fill="black"
+                        ></circle>
+                        <circle
+                          cx="5.73583"
+                          cy="1.61321"
+                          r="1.5"
+                          fill="black"
+                        ></circle>
+                        <circle
+                          cx="5.73583"
+                          cy="5.5566"
+                          r="1.5"
+                          fill="black"
+                        ></circle>
+                        <circle
+                          cx="9.85851"
+                          cy="5.5566"
+                          r="1.5"
+                          fill="black"
+                        ></circle>
+                        <circle
+                          cx="9.85851"
+                          cy="9.5"
+                          r="1.5"
+                          fill="black"
+                        ></circle>
+                        <circle
+                          cx="13.9811"
+                          cy="9.5"
+                          r="1.5"
+                          fill="black"
+                        ></circle>
+                        <circle
+                          cx="5.73583"
+                          cy="13.4434"
+                          r="1.5"
+                          fill="black"
+                        ></circle>
+                        <circle
+                          cx="9.85851"
+                          cy="13.4434"
+                          r="1.5"
+                          fill="black"
+                        ></circle>
+                        <circle
+                          cx="1.61321"
+                          cy="17.3868"
+                          r="1.5"
+                          fill="black"
+                        ></circle>
+                        <circle
+                          cx="5.73583"
+                          cy="17.3868"
+                          r="1.5"
+                          fill="black"
+                        ></circle>
+                      </svg>
+                    </span>
+                  </a>
+                  <a href="https://thirumurugan7.hashnode.dev/spin-up-an-onchain-game-with-zk-spin?ref=twitter-share" target="_blank" rel="noopener noreferrer" class="Btn-Container">
+                    <span class="text">Docs</span>
+                    <span class="icon-Container">
+                      <svg
+                        width="16"
+                        height="19"
+                        viewBox="0 0 16 19"
+                        fill="nones"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <circle
+                          cx="1.61321"
+                          cy="1.61321"
+                          r="1.5"
+                          fill="black"
+                        ></circle>
+                        <circle
+                          cx="5.73583"
+                          cy="1.61321"
+                          r="1.5"
+                          fill="black"
+                        ></circle>
+                        <circle
+                          cx="5.73583"
+                          cy="5.5566"
+                          r="1.5"
+                          fill="black"
+                        ></circle>
+                        <circle
+                          cx="9.85851"
+                          cy="5.5566"
+                          r="1.5"
+                          fill="black"
+                        ></circle>
+                        <circle
+                          cx="9.85851"
+                          cy="9.5"
+                          r="1.5"
+                          fill="black"
+                        ></circle>
+                        <circle
+                          cx="13.9811"
+                          cy="9.5"
+                          r="1.5"
+                          fill="black"
+                        ></circle>
+                        <circle
+                          cx="5.73583"
+                          cy="13.4434"
+                          r="1.5"
+                          fill="black"
+                        ></circle>
+                        <circle
+                          cx="9.85851"
+                          cy="13.4434"
+                          r="1.5"
+                          fill="black"
+                        ></circle>
+                        <circle
+                          cx="1.61321"
+                          cy="17.3868"
+                          r="1.5"
+                          fill="black"
+                        ></circle>
+                        <circle
+                          cx="5.73583"
+                          cy="17.3868"
+                          r="1.5"
+                          fill="black"
+                        ></circle>
+                      </svg>
+                    </span>
+                  </a>
+                </div>
+              </div>
             </div>
           </div>
           <div className="-mr-2 flex md:hidden">
@@ -121,6 +271,7 @@ const Navbar = () => {
         </div>
       </div>
 
+      {/* Mobile */}
       <div
         className={`${isOpen ? "block" : "hidden"} md:hidden`}
         id="mobile-menu"
@@ -164,6 +315,136 @@ const Navbar = () => {
           >
             Contact Us
           </a>
+          <div className=" px-2">
+            <div className=" space-y-4">
+              <a href="https://github.com/m4-team/spin-sdk/blob/main/guide/2_INTRODUCTION.md" target="_blank" rel="noopener noreferrer" class="Btn-Container">
+                <span class="text">Github</span>
+                <span class="icon-Container">
+                  <svg
+                    width="16"
+                    height="19"
+                    viewBox="0 0 16 19"
+                    fill="nones"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <circle
+                      cx="1.61321"
+                      cy="1.61321"
+                      r="1.5"
+                      fill="black"
+                    ></circle>
+                    <circle
+                      cx="5.73583"
+                      cy="1.61321"
+                      r="1.5"
+                      fill="black"
+                    ></circle>
+                    <circle
+                      cx="5.73583"
+                      cy="5.5566"
+                      r="1.5"
+                      fill="black"
+                    ></circle>
+                    <circle
+                      cx="9.85851"
+                      cy="5.5566"
+                      r="1.5"
+                      fill="black"
+                    ></circle>
+                    <circle cx="9.85851" cy="9.5" r="1.5" fill="black"></circle>
+                    <circle cx="13.9811" cy="9.5" r="1.5" fill="black"></circle>
+                    <circle
+                      cx="5.73583"
+                      cy="13.4434"
+                      r="1.5"
+                      fill="black"
+                    ></circle>
+                    <circle
+                      cx="9.85851"
+                      cy="13.4434"
+                      r="1.5"
+                      fill="black"
+                    ></circle>
+                    <circle
+                      cx="1.61321"
+                      cy="17.3868"
+                      r="1.5"
+                      fill="black"
+                    ></circle>
+                    <circle
+                      cx="5.73583"
+                      cy="17.3868"
+                      r="1.5"
+                      fill="black"
+                    ></circle>
+                  </svg>
+                </span>
+              </a>
+              <a href="https://thirumurugan7.hashnode.dev/spin-up-an-onchain-game-with-zk-spin?ref=twitter-share" target="_blank" rel="noopener noreferrer" class="Btn-Container">
+                <span class="text">Docs</span>
+                <span class="icon-Container">
+                  <svg
+                    width="16"
+                    height="19"
+                    viewBox="0 0 16 19"
+                    fill="nones"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <circle
+                      cx="1.61321"
+                      cy="1.61321"
+                      r="1.5"
+                      fill="black"
+                    ></circle>
+                    <circle
+                      cx="5.73583"
+                      cy="1.61321"
+                      r="1.5"
+                      fill="black"
+                    ></circle>
+                    <circle
+                      cx="5.73583"
+                      cy="5.5566"
+                      r="1.5"
+                      fill="black"
+                    ></circle>
+                    <circle
+                      cx="9.85851"
+                      cy="5.5566"
+                      r="1.5"
+                      fill="black"
+                    ></circle>
+                    <circle cx="9.85851" cy="9.5" r="1.5" fill="black"></circle>
+                    <circle cx="13.9811" cy="9.5" r="1.5" fill="black"></circle>
+                    <circle
+                      cx="5.73583"
+                      cy="13.4434"
+                      r="1.5"
+                      fill="black"
+                    ></circle>
+                    <circle
+                      cx="9.85851"
+                      cy="13.4434"
+                      r="1.5"
+                      fill="black"
+                    ></circle>
+                    <circle
+                      cx="1.61321"
+                      cy="17.3868"
+                      r="1.5"
+                      fill="black"
+                    ></circle>
+                    <circle
+                      cx="5.73583"
+                      cy="17.3868"
+                      r="1.5"
+                      fill="black"
+                    ></circle>
+                  </svg>
+                </span>
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </nav>
