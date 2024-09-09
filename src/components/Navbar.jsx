e6366c26db12982fd0841b1115af5a1c239280a6
vishalmet <vishalmet003@gmail.com>
2024-09-09 09:47:21 +0530

    fixed and backdrop

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,8 +16,8 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="gradient-container overflow-x-hidden epilogue text-white">
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+    <nav className="bg-black/70 backdrop-filter backdrop-blur-md overflow-x-hidden epilogue text-white w-full fixed z-50">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 ">
         <div className="flex items-center justify-between h-20">
           <div className="flex items-center">
             <Link to="/" className="text-lg font-bold">
