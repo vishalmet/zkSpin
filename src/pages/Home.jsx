aa2b6e9c7e701ba503ae1676d82b9e416d216b41
vishalmet <vishalmet003@gmail.com>
2024-09-13 20:21:16 +0530

    changed hover and filled spaces

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,43 +11,45 @@ import Partners from "../components/Partners";
 const Home = () => {
   return (
     <div className="bg-[#141414] text-white epilogue">
-      <div className="gradient-container overflow-x-hidden min-h-full pb-4 md:pb-0 md:min-h-screen space-grotesk text-3xl md:text-5xl lg:text-9xl font-medium uppercase">
+      <div className="gradient-container overflow-x-hidden min-h-full pb-4 md:pb-0 md:min-h-screen space-grotesk text-3xl md:text-5xl lg:text-9xl font-medium ">
         <div className=" pt-24 md:pt-32 ml-4 md:ml-[72px] flex justify-between items-center">
-          <div className="">
-          <p className="  ">Infinitely</p>
-          <div className=" flex items-center">
-            <img
-              className="w-[100px] md:w-[260px] md:h-[84px] mr-4 md:mr-10"
-              src={Shapes}
-              alt="shapes"
-            />
-            <p>scalable</p>
+          <div className="uppercase">
+            <p className="  ">Infinitely</p>
+            <div className=" flex items-center">
+              <img
+                className="w-[100px] md:w-[260px] md:h-[84px] mr-4 md:mr-10"
+                src={Shapes}
+                alt="shapes"
+              />
+              <p>scalable</p>
+            </div>
+            <p>
+              on-chain game <br /> engine
+            </p>
           </div>
-          <p>
-            on-chain game <br /> engine
-          </p>
-          </div>
-          <div className=" ">
-            <img className=" h-[160px] md:h-[520px] w-[100px] md:w-[200px]" src={LandingRight} alt="Game" />
+          <div className="w-[100px] md:w-[200px] lg:w-[400px] overflow-hidden ">
+            <img className=" h-[160px] md:h-[520px] w-[100px] md:w-[200px] lg:w-[400px] object-cover transform transition-transform duration-500 hover:scale-110" src={LandingRight} alt="Game" />
           </div>
         </div>
 
         <div className=" pt-10 md:pt-20 md:flex">
-          <img
-            className="h-[130px] md:h-[280px] w-full md:w-[800px]"
-            src={Game1}
-            alt=""
-          />
-          <div className="  mx-auto">
-            <p className=" pt-4 md:pt-0 text-xl mx-4 flex justify-center md:mx-0 md:text-[28px] font-medium">
+          <div className=" w-full md:w-[800px] overflow-hidden">
+            <img
+              className="h-[130px] md:h-[280px] w-full md:w-[800px] flex-shrink object-cover transform transition-transform duration-500 hover:scale-110"
+              src={Game1}
+              alt=""
+            />
+          </div>
+          <div className=" mx-auto">
+            <p className=" pt-4 md:pt-0 text-xl md:text-[28px] font-medium">
               Unlimited TPS  •  Gasless  •  Ultra-low Latency
             </p>
-            <div className=" flex justify-center pt-3 md:pt-6">
+            <div className=" flex justify-start pt-3 md:pt-6">
               <a
                 href="https://github.com/m4-team/spin-sdk/blob/main/guide/2_INTRODUCTION.md"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="flex items-center cursor-pointer text-base md:text-2xl font-semibold bg-gradient-to-br from-customStart to-customEnd hover:bg-gradient-to-tl hover:scale-95 p-3 px-5"
+                className="flex items-center cursor-pointer text-base md:text-xl font-semibold bg-gradient-to-br from-customStart to-customEnd hover:bg-gradient-to-tl hover:scale-95 p-3 px-5"
               >
                 GET STARTED
                 <svg
@@ -69,7 +71,7 @@ const Home = () => {
           </div>
         </div>
       </div>
-      
+
       <ZkTech />
       <SpinIntelligence />
       <RoadMap />
