72cf02c75e0ce73abf7a2e5e974e82265521d5d8
vishalmet <vishalmet003@gmail.com>
2024-09-13 22:55:40 +0530

    routing for contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React, { Suspense, lazy } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
-import Logo from "./assets/logo.png"
+import Logo from "./assets/icon.png"
 
 // Lazy load pages
 const Home = lazy(() => import('./pages/Home'))
 const SpinPlayground = lazy(() => import('./pages/Playground'))
 const Games = lazy(() => import('./pages/Game'))
+const Contact = lazy(() => import('./pages/Contact'))
 
 const App = () => {
   return (
@@ -14,13 +15,14 @@ const App = () => {
       <Router>
         {/* Suspense is used to display a fallback while lazy-loaded components are being fetched */}
         <Suspense fallback={<div className="flex justify-center items-center h-screen bg-black">
-          <img className=' animate-pulse' src={Logo} alt="" />
+          <img className=' animate-spin' src={Logo} alt="" />
         </div>}>
         <Navbar />
           <Routes>
             <Route path='/' element={<Home />}></Route>
             <Route path='/games' element={<Games />}></Route>
             <Route path='/playground' element={<SpinPlayground />}></Route>
+            <Route path='/contactus' element={<Contact />}></Route>
           </Routes>
         </Suspense>
       </Router>
