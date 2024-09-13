9e938b5b172e4701e2491fc562e9b0fd9c37fbac
vishalmet <vishalmet003@gmail.com>
2024-09-13 22:57:37 +0530

    hyperlinks

--- src/components/Partners.jsx
+++ src/components/Partners.jsx
@@ -18,103 +18,141 @@ const Partners = () => {
       </p>
       <div className="hidden md:block h-full py-10 p-4 md:p-16 items-center">
         <div className=" grid grid-cols-2 md:grid-cols-4 mx-16 items-center">
+          <a href="https://a16zcrypto.com/accelerator/" rel="noopener noreferrer" target="_blank">
+            <img
+              className="md:w-[150px] md:h-[60px] flex justify-center mx-auto hover:scale-105"
+              src={CSX}
+              alt="A16Z"
+            />
+          </a>
+          <a href="https://scroll.io/" rel="noopener noreferrer" target="_blank">
+            <img
+              className=" md:w-[240px] md:h-[110px] flex justify-center mx-auto hover:scale-105"
+              src={Scroll}
+              alt="SCROLL"
+            />
+          </a>
+          <a href="" rel="noopener noreferrer" target="_blank">
+            <img
+              className="md:w-[240px] md:h-[110px] flex justify-center mx-auto hover:scale-105"
+              src={ABGA}
+              alt="ABGA"
+            />
+          </a>
+          <a href="https://www.certik.com/" rel="noopener noreferrer" target="_blank">
+            <img
+              className="md:w-[240px] md:h-[110px] flex justify-center mx-auto hover:scale-105"
+              src={Certik}
+              alt="CERTIK"
+            />
+          </a>
+        </div>
+        <div className=" md:flex justify-center space-x-6 items-center">
+          <a href="https://cysic.xyz/" rel="noopener noreferrer" target="_blank">
+            <img
+              className="md:w-[240px] md:h-[110px] flex justify-center mx-auto hover:scale-105"
+              src={Cysic}
+              alt="CYSIC"
+            />
+          </a>
+          <a href="https://koyamaki.io/about.html" rel="noopener noreferrer" target="_blank">
+            <img
+              className="md:w-[240px] md:h-[110px] flex justify-center mx-auto hover:scale-105"
+              src={Koyamaki}
+              alt="KOYAMAKI"
+            />
+          </a>
+          <a href="https://delphinuslab.com/" rel="noopener noreferrer" target="_blank">
+            <img
+              className="md:w-[240px] md:h-[110px] flex justify-center mx-auto hover:scale-105"
+              src={Delphinus}
+              alt="DELPHINUS"
+            />
+          </a>
+          <a href="https://www.lita.foundation/" rel="noopener noreferrer" target="_blank">
+            <img
+              className="md:w-[240px] md:h-[110px] flex justify-center mx-auto hover:scale-105"
+              src={Lita}
+              alt="LITA"
+            />
+          </a>
+          <a href="" rel="noopener noreferrer" target="_blank">
+            <img
+              className="md:w-[240px] md:h-[110px] flex justify-center mx-auto hover:scale-105"
+              src={BGA}
+              alt="BGA"
+            />
+          </a>
+        </div>
+      </div>
+
+
+      {/* mobile */}
+      <div className=" grid grid-cols-2 items-center md:hidden">
+        <a href="https://a16zcrypto.com/accelerator/" rel="noopener noreferrer" target="_blank">
           <img
-            className="md:w-[150px] md:h-[60px] flex justify-center mx-auto hover:scale-105"
+            className=" h-[50px] w-[127px] flex justify-center mx-auto hover:scale-105"
             src={CSX}
             alt="A16Z"
           />
+        </a>
+        <a href="https://scroll.io/" rel="noopener noreferrer" target="_blank">
           <img
-            className=" md:w-[240px] md:h-[110px] flex justify-center mx-auto hover:scale-105"
+            className=" h-[100px] w-[167px] flex justify-center mx-auto hover:scale-105"
             src={Scroll}
             alt="SCROLL"
           />
+        </a>
+        <a href="" rel="noopener noreferrer" target="_blank">
           <img
-            className="md:w-[240px] md:h-[110px] flex justify-center mx-auto hover:scale-105"
+            className=" h-[100px] w-[167px] flex justify-center mx-auto hover:scale-105"
             src={ABGA}
             alt="ABGA"
           />
+        </a>
+        <a href="https://www.certik.com/" rel="noopener noreferrer" target="_blank">
           <img
-            className="md:w-[240px] md:h-[110px] flex justify-center mx-auto hover:scale-105"
+            className="h-[100px] w-[167px] flex justify-center mx-auto hover:scale-105"
             src={Certik}
             alt="CERTIK"
           />
-        </div>
-        <div className=" md:flex justify-center space-x-6 items-center">
+        </a>
+        <a href="https://cysic.xyz/" rel="noopener noreferrer" target="_blank">
           <img
-            className="md:w-[240px] md:h-[110px] flex justify-center mx-auto hover:scale-105"
+            className=" h-[100px] w-[167px] flex justify-center mx-auto hover:scale-105"
             src={Cysic}
             alt="CYSIC"
           />
+        </a>
+        <a href="https://koyamaki.io/about.html" rel="noopener noreferrer" target="_blank">
           <img
-            className="md:w-[240px] md:h-[110px] flex justify-center mx-auto hover:scale-105"
+            className=" h-[100px] w-[167px] flex justify-center mx-auto hover:scale-105"
             src={Koyamaki}
             alt="KOYAMAKI"
           />
+        </a>
+        <a href="https://delphinuslab.com/" rel="noopener noreferrer" target="_blank">
           <img
-            className="md:w-[240px] md:h-[110px] flex justify-center mx-auto hover:scale-105"
+            className="h-[100px] w-[167px] flex justify-center mx-auto hover:scale-105"
             src={Delphinus}
             alt="DELPHINUS"
           />
+        </a>
+        <a href="https://www.lita.foundation/" rel="noopener noreferrer" target="_blank">
           <img
-            className="md:w-[240px] md:h-[110px] flex justify-center mx-auto hover:scale-105"
+            className="h-[100px] w-[167px] flex justify-center mx-auto hover:scale-105"
             src={Lita}
             alt="LITA"
           />
-          <img
-            className="md:w-[240px] md:h-[110px] flex justify-center mx-auto hover:scale-105"
-            src={BGA}
-            alt="BGA"
-          />
-        </div>
+        </a>
       </div>
-      {/* mobile */}
-      <div className=" grid grid-cols-2 items-center md:hidden">
-        <img
-          className=" h-[50px] w-[127px] flex justify-center mx-auto hover:scale-105"
-          src={CSX}
-          alt="A16Z"
-        />
-        <img
-          className=" h-[100px] w-[167px] flex justify-center mx-auto hover:scale-105"
-          src={Scroll}
-          alt="SCROLL"
-        />
-        <img
-          className=" h-[100px] w-[167px] flex justify-center mx-auto hover:scale-105"
-          src={ABGA}
-          alt="ABGA"
-        />
-        <img
-          className="h-[100px] w-[167px] flex justify-center mx-auto hover:scale-105"
-          src={Certik}
-          alt="CERTIK"
-        />
-        <img
-          className=" h-[100px] w-[167px] flex justify-center mx-auto hover:scale-105"
-          src={Cysic}
-          alt="CYSIC"
-        />
-        <img
-          className=" h-[100px] w-[167px] flex justify-center mx-auto hover:scale-105"
-          src={Koyamaki}
-          alt="KOYAMAKI"
-        />
-        <img
-          className="h-[100px] w-[167px] flex justify-center mx-auto hover:scale-105"
-          src={Delphinus}
-          alt="DELPHINUS"
-        />
-        <img
-          className="h-[100px] w-[167px] flex justify-center mx-auto hover:scale-105"
-          src={Lita}
-          alt="LITA"
-        />
-      </div>
+      <a href="" rel="noopener noreferrer" target="_blank">
         <img
           className="h-[100px] md:hidden w-[167px] flex justify-center mx-auto hover:scale-105"
           src={BGA}
           alt="BGA"
         />
+        </a>
     </div>
   );
 };
