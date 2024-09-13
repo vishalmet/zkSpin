4baad9824540db08bb6c4e36d092519659da5d7e
vishalmet <vishalmet003@gmail.com>
2024-09-13 21:20:31 +0530

    suspense

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,28 @@
-import React from 'react'
+import React, { Suspense, lazy } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import SpinPlayground from './pages/Playground'
 import Navbar from './components/Navbar'
-import Games from './pages/Game'
+import Logo from "./assets/logo.png"
 
+// Lazy load pages
+const Home = lazy(() => import('./pages/Home'))
+const SpinPlayground = lazy(() => import('./pages/Playground'))
+const Games = lazy(() => import('./pages/Game'))
 
 const App = () => {
   return (
     <div>
       <Router>
+        {/* Suspense is used to display a fallback while lazy-loaded components are being fetched */}
+        <Suspense fallback={<div className="flex justify-center items-center h-screen bg-black">
+          <img className=' animate-pulse' src={Logo} alt="" />
+        </div>}>
         <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />}></Route>
-          <Route path='/games' element={<Games />}></Route>
-          <Route path='/playground' element={<SpinPlayground />}></Route>
-        </Routes>
+          <Routes>
+            <Route path='/' element={<Home />}></Route>
+            <Route path='/games' element={<Games />}></Route>
+            <Route path='/playground' element={<SpinPlayground />}></Route>
+          </Routes>
+        </Suspense>
       </Router>
     </div>
   )
